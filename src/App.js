090217d2e93cda6e23a87e2a61370c0e24a5f4ba
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-10-11 13:31:32 +0200

    added next and previous review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
-
+import Card from './component/Card';
+import data from './data'
+import {React,useState} from 'react';
 function App() {
+  const [index,setIndex] = useState(1)
+  
+  function nextIndex(){
+
+    setIndex(prevIndex => {
+
+      if(prevIndex > data.length-2)
+      return  0
+      else
+      return  index+1
+    })
+  }
+  function prevIndex(){
+    
+    setIndex(prevIndex => {
+      if(prevIndex === 0)
+      return  data.length-1
+      else
+      return  index-1
+  })}
+
   return (
     <div className="App">
-      grrr  
+      <h2>Our Reviews</h2>
+      <div className='line'></div>
+      <Card {...data[index]} prev={prevIndex} next={nextIndex}/> 
+      {//data.map(p =>
+         //<Card {...p}/>
+      //)
+      }
     </div>
   );
 }
---
+++ src/component/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Card.css'
+import {MdOutlineReviews} from 'react-icons/md'
+import {AiOutlineLeft,AiOutlineRight} from 'react-icons/ai'
+export default function Card({id,name,job,image,text,prev,next}) {
+  return (
+    <div className='card'>
+
+        <img src={image} alt="person" className="image" />
+        <div className="symbol"><MdOutlineReviews/></div>
+        <div className="name">{name}</div>
+        <div className="job">{job}</div>
+        <div className="details">{text}</div>
+        <div className="arrows">
+            <button className="btn" onClick={prev}><AiOutlineLeft/></button>
+            <button className="btn" onClick={next}><AiOutlineRight/></button>
+        </div>
+       
+        <button className="suprise">Suprise</button>
+    </div>
+  )
+}
